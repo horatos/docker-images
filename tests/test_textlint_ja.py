8345e08111eacbc8ba2197c1d4549c5f71ceaf9a
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2022-08-06 22:06:39 +0900

    refactor: add type hints

--- tests/test_textlint_ja.py
+++ tests/test_textlint_ja.py
@@ -1,32 +1,38 @@
-import os
 from contextlib import contextmanager
+import os
+from typing import Union
 
 import docker
 from docker.types import Mount
 from pytest import fixture
 
 
-IMAGE_NAME='docker-images_textlint-ja'
-HOST_PWD=os.environ['HOST_PWD']
+IMAGE_NAME: str = 'docker-images_textlint-ja'
+HOST_PWD: str = os.environ['HOST_PWD']
 
 
 @fixture
-def client():
+def client() -> docker.DockerClient:
     return docker.from_env()
 
 
 @fixture
-def mount_test_dir():
+def mount_test_dir() -> Mount:
     return Mount(source=f'{HOST_PWD}/tests', target='/home/node/app', type='bind')
 
 
 @fixture
-def mount_data_tex_dir():
+def mount_data_tex_dir() -> Mount:
     return Mount(source=f'{HOST_PWD}/data/tex', target='/home/node/app', type='bind')
 
 
 @contextmanager
-def run_container(client, image, command=None, **kwargs):
+def run_container(
+        client: docker.DockerClient,
+        image: str,
+        command: Union[list[str], None] = None,
+        **kwargs
+) -> docker.models.containers.Container:
     container = client.containers.run(image, command, detach=True, **kwargs)
     try:
         yield container
@@ -34,7 +40,7 @@ def run_container(client, image, command=None, **kwargs):
         container.remove()
 
 
-def test_with_no_input(client):
+def test_with_no_input(client: docker.DockerClient):
     """何も入力を渡さないときに正常に終了することを確認する"""
     with run_container(client, IMAGE_NAME) as container:
         result = container.wait()
@@ -44,16 +50,17 @@ def test_with_no_input(client):
     assert result['StatusCode'] == 0
 
 
-def test_ls(client, mount_test_dir):
+def test_ls(client: docker.DockerClient, mount_test_dir: Mount):
     """lsコマンドを実行してマウント位置が正しいことを確認する"""
     with run_container(client, IMAGE_NAME, entrypoint="bash -c 'ls -a'", mounts=[mount_test_dir]) as container:
         result = container.wait()
         file_list = container.logs().decode().splitlines()
 
     assert 'test_textlint_ja.py' in file_list
+    assert result['StatusCode'] == 0
 
 
-def test_md_ja(client, mount_test_dir):
+def test_md_ja(client: docker.DockerClient, mount_test_dir: Mount):
     """Markdownファイルのlintができることを確認する"""
     with run_container(client, IMAGE_NAME, ['--preset', 'preset-japanese', 'data/ja.md'], mounts=[mount_test_dir]) as container:
         result = container.wait()
@@ -63,9 +70,9 @@ def test_md_ja(client, mount_test_dir):
     assert result['StatusCode'] == 0
 
 
-def test_latex_ja(client, mount_data_tex_dir):
+def test_latex_ja(client: docker.DockerClient, mount_data_tex_dir: Mount):
     """LaTeXファイルのlintができることを確認する"""
-    with run_container(client, IMAGE_NAME, '*.tex', mounts=[mount_data_tex_dir]) as container:
+    with run_container(client, IMAGE_NAME, ['*.tex'], mounts=[mount_data_tex_dir]) as container:
         result = container.wait()
         logs = container.logs().decode()
 
